f5d0da75b1cedb137ae301cdb36d140132a30b97
Radhebhaixyz <Sudama703749@gmail.com>
2025-04-15 22:41:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 29946578
 api_hash = "57e0b762f105ab1db072fabe4d65114b"
-bot_token = "7766330338:AAHJD0Sv8A4f0GdIuXkqhU3iSoMVzybtAqA"
+bot_token = "8017825846:AAHRBis3bOjAyTEmWke3vGaxwv33O6730mw"
 auth_users = "7731407856"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "7731407856"
